fda572206bd30a04693a62e8f18ee57a2d8114f9
João Gabriel Capitaneo <joaocapitaneo@gmail.com>
2020-09-30 16:07:12 -0300

    ação com touch no menu
    adicionado texto na parte inferior da tela relaconado ao touch no item menu.

--- app/src/main/java/com/example/toolbar/MainActivity.java
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +18,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar= findViewById(R.id.toolbar_main);//adicionado
+        toolbar = findViewById(R.id.toolbar_main);//adicionado
         setSupportActionBar(toolbar);//adicionado
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.search_menu:
+                Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.carrinho_menu:
+                Toast.makeText(this, "Carrinho", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.config_menu:
+                Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about_menu:
+                Toast.makeText(this, "Sobre", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+            return super.onOptionsItemSelected(item);
+    }
+
 }
+
+
